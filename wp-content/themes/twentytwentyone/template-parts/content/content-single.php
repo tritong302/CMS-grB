b4324fab3a96f51063077b1cbb5355769744a4d5
tritong302 <tritong302@gmail.com>
2024-11-01 16:16:04 +0700

    module6_detail

--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -11,34 +11,37 @@
 
 ?>
 
+<?php     
+$date = get_the_date('d');
+$month = get_the_date('m');
+$year = get_the_date('y');
+?>
+<div class="detailpost">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header alignwide">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php twenty_twenty_one_post_thumbnail(); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
-
-	<?php if ( ! is_singular( 'attachment' ) ) : ?>
-		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
-	<?php endif; ?>
+    <header class="entry-header alignwide">
+        <?php the_title( '<div class="row">
+        <div class="ten col-md-10 col-xs-9"><h1 class="ten1 entry-title detail-title">', '</h1></div>
+        <div class="col-md-2 col-xs-3 m-auto"><div class="headlinesdate date-detail">
+        <div class="headlinesdm">
+            <div class="headlinesday">'. $date .'</div>
+            <div class="headlinesmonth">'. $month .'</div>
+        </div>
+        <div class="headlinesyear">’'. $year .'</div>
+        </div></div>
+        </div>' ); ?>
+        <div class="row">
+            <div class="col-md-12"><div class="overviewline"></div></div>
+        </div>
+        <?php twenty_twenty_one_post_thumbnail(); ?>
+    </header><!-- .entry-header -->
+    <div class="entry-content" style="margin-bottom: 20px;">
+        <?php the_content(); ?>
+    </div><!-- .entry-content -->
+    <?php if ( ! is_singular( 'attachment' ) ) : ?>
+        <?php get_template_part( 'template-parts/post/author-bio' ); ?>
+    <?php endif; ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
+       
+        </div>

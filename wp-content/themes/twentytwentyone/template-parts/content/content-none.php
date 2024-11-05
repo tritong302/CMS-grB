49f12dce18449de5a657c4328c52fa7ac7e4a979
Tri Tổng <tritong302@gmail.com>
2024-11-05 16:18:13 +0700

    module4_search

--- wp-content/themes/twentytwentyone/template-parts/content/content-none.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-none.php
@@ -9,58 +9,93 @@
  * @since Twenty Twenty-One 1.0
  */
 
+
+$twentytwentyone_aria_label = 'aria-label="Search form"';
+
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header alignwide">
-		<?php if ( is_search() ) : ?>
+    <header class="page-header alignwide">
+        <?php if ( is_search() ) : ?>
 
-			<h1 class="page-title">
-				<?php
-				printf(
-					/* translators: %s: Search term. */
-					esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-					'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-				);
-				?>
-			</h1>
+            <h1 class="page-title">
+                <?php
+                printf(
+                    /* translators: %s: Search term. */
+                    esc_html__( 'Search: %s', 'twentytwentyone' ),
+                    '<span class="page-description search-term">' .'"'. esc_html( get_search_query() ) .'"' . '</span>'
+                );
+                ?>
+                <?php if ( !have_posts() ) {?>
+                    <p><?php esc_html_e( 'We could not find any results for your search. You can give it another try through the search form below.', 'twentytwentyone' ); ?></p>
+                <?php }?>
+            </h1>
 
-		<?php else : ?>
+        <?php else : ?>
 
-			<h1 class="page-title"><?php esc_html_e( 'Nothing here', 'twentytwentyone' ); ?></h1>
+            <h1 class="page-title"><?php esc_html_e( 'Nothing here', 'twentytwentyone' ); ?></h1>
 
-		<?php endif; ?>
-	</header><!-- .page-header -->
+        <?php endif; ?>
+    </header><!-- .page-header -->
 
-	<div class="page-content default-max-width">
+    <div class="page-content default-max-width">
 
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+        <?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<?php
-			printf(
-				'<p>' . wp_kses(
-					/* translators: %s: Link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwentyone' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
-			?>
+            <?php
+            printf(
+                '<p>' . wp_kses(
+                    /* translators: %s: Link to WP admin new post page. */
+                    __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwentyone' ),
+                    array(
+                        'a' => array(
+                            'href' => array(),
+                        ),
+                    )
+                ) . '</p>',
+                esc_url( admin_url( 'post-new.php' ) )
+            );
+            ?>
 
-		<?php elseif ( is_search() ) : ?>
+        <?php elseif ( is_search() ) : ?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentytwentyone' ); ?></p>
-			<?php get_search_form(); ?>
+            <form role="search" <?php echo $twentytwentyone_aria_label; ?> method="get" class="search-form card card-sm" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <div class="card-body row no-gutters align-items-center">
+                    <div class="col-auto">
+                        <i class="fas fa-search h4 text-body"></i>
+                    </div>
+                    <!--end of col-->
+                    <div class="col">
+                        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords">
+                    </div>
+                    <!--end of col-->
+                    <div class="col-auto">
+                        <button class="btn btn-lg btn-success" type="submit">Search</button>
+                    </div>
+                    <!--end of col-->
+                </div>
+            </form>
+            
+        <?php else : ?>
 
-		<?php else : ?>
+            <p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwentyone' ); ?></p>
+            <form role="search" <?php echo $twentytwentyone_aria_label; ?> method="get" class="search-form card card-sm" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <div class="card-body row no-gutters align-items-center">
+                    <div class="col-auto">
+                        <i class="fas fa-search h4 text-body"></i>
+                    </div>
+                    <!--end of col-->
+                    <div class="col">
+                        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords">
+                    </div>
+                    <!--end of col-->
+                    <div class="col-auto">
+                        <button class="btn btn-lg btn-success" type="submit">Search</button>
+                    </div>
+                    <!--end of col-->
+                </div>
+            </form>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwentyone' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php endif; ?>
-	</div><!-- .page-content -->
+        <?php endif; ?>
+    </div><!-- .page-content -->
 </section><!-- .no-results -->

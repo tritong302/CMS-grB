878ddbb02da884c3c4416a557587ba04fcb475f3
VitTrang <hanphi12022003@gmail.com>
2024-11-07 20:57:40 +0700

    module3_FooterFix

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db-test' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
 define( 'DB_USER', 'root' );

af5cc3e73fbdccd23d38d8a85c5b14372c2d30a1
Tri Tổng <tritong302@gmail.com>
2024-11-08 13:01:09 +0700

    Toiuu

--- wp-content/themes/twentytwentyone/search.php
+++ wp-content/themes/twentytwentyone/search.php
@@ -24,7 +24,42 @@ get_header();
 
 		</div>
 		<div class="col-md-6">
+			<?php
+			// Start the Loop.
+			while (have_posts()) {
+				the_post();
 
+				/*
+						* Include the Post-Format-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						*/
+				echo '<div class="list-view-search">';
+				echo '<div class="row">';
+				echo '<div class="col-md-5">';
+
+				if (is_search()) {
+					echo '<div class="img-detail-search top_news_block_thumb">';
+					twenty_twenty_one_post_thumbnail();
+					echo '</div>';
+				}
+				echo '</div>';
+
+				echo '<div class="col-md-7 top_news_block_desc">';
+				echo '<div class="content-detail-search">';
+				get_template_part('template-parts/content/content-excerpt', get_post_format());
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
+
+				// Hiển thị nội dung bài viết với class.
+			} // End the Loop.
+			// Previous/next page navigation.
+			twenty_twenty_one_the_posts_navigation();
+
+			// If no content, include the "No posts found" template.
+			?>
 		</div>
 
 
@@ -66,44 +101,7 @@ get_header();
 				</ul>
 			</div>
 		</div>
-		<div class="col-md-6">
-			<?php
-			// Start the Loop.
-			while (have_posts()) {
-				the_post();
 
-				/*
-						* Include the Post-Format-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						*/
-				echo '<div class="list-view-search">';
-				echo '<div class="row">';
-				echo '<div class="col-md-5">';
-
-				if (is_search()) {
-					echo '<div class="img-detail-search top_news_block_thumb">';
-					twenty_twenty_one_post_thumbnail();
-					echo '</div>';
-				}
-				echo '</div>';
-
-				echo '<div class="col-md-7 top_news_block_desc">';
-				echo '<div class="content-detail-search">';
-				get_template_part('template-parts/content/content-excerpt', get_post_format());
-				echo '</div>';
-				echo '</div>';
-				echo '</div>';
-				echo '</div>';
-
-				// Hiển thị nội dung bài viết với class.
-			} // End the Loop.
-			// Previous/next page navigation.
-			twenty_twenty_one_the_posts_navigation();
-
-			// If no content, include the "No posts found" template.
-			?>
-		</div>
 	</div>
 
 </div>
